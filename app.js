81a18da9af8ce9d05ba2a3bf8ff217d9dc63b9f1
luqi <luqi@derbysoft.com>
2018-07-23 14:26:03 +0800

    delete useless files

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const importRouter = require('./routes/import');
 const ejs = require("ejs");
 
 var app = express();
 
-app.locals.constsjs = require('./routes/consts');
 app.locals.title = "my title";
 
 // view engine setup
