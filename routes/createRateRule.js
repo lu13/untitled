86cf5d713f8d5126ffe62ac5fbe466462f98a48b
luqi <luqi@derbysoft.com>
2018-12-13 14:42:58 +0800

    add create rate rule by multiple markets

--- routes/createRateRule.js
+++ routes/createRateRule.js
@@ -5,7 +5,7 @@ router.get('/', function (req, res) {
     res.render("createRateRule");
 });
 
-router.post('/createFile', function (req, res) {
+router.post('/createFileSingleCountry', function (req, res) {
     let rslt = '<PrivateRates>';
     let prefix = req.body.prefix;
     let codes = req.body.countryCodes;
@@ -20,6 +20,32 @@ router.post('/createFile', function (req, res) {
                     </UserRateCondition>
                   </RateRule>
 `;
+    });
+    rslt += '</PrivateRates>';
+
+    res.writeHead(200, {'Content-Type': 'application/xml'});
+    res.end(rslt);
+
+});
+
+router.post('/createFileCountry', function (req, res) {
+    let rslt = '<PrivateRates>';
+    let prefix = req.body.prefix;
+    let codes = req.body.countryCodes;
+    let marketAry = req.body.market;
+    let codeAry = codes.split(';');
+    codeAry.forEach(function (codes, i) {
+        let market = marketAry.split(';')[i];
+        rslt += ` <RateRule id="${prefix}_${market}">
+                    <UserRateCondition op="any">
+                        <UserDeviceType>mobile</UserDeviceType>
+                        <UserDeviceType>desktop</UserDeviceType>
+		            	<UserDeviceType>tablet</UserDeviceType>`;
+        codes.split(',').forEach(function (code) {
+            rslt += `<UserCountry>${code}</UserCountry>`;
+        });
+        rslt += `</UserRateCondition>
+                  </RateRule>`;
     });
     rslt += '</PrivateRates>';
 
